548aa0a5f660903f9db4ac196620f11448783ce2
Ben Morris <bensan.morris@googlemail.com>
2024-07-22 17:55:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ten random ints
+
+Write an application which iterates a container of objects calling a MakeSound() method for
+each. There should 3 objects in the container when it is iterated, an instance of three classes
+called Dog, Sheep and Lion. The MakeSound() method should print to screen “Woof”, “Baaa”
+and “Roar” respectively for each of the three classes.
+```
+(windows)
+cmake -B build -G "Visual Studio 16 2019" .
+cd build
+cmake --build . --config Release
+.\build\Release\make_sound.exe
+
+(linux)
+cmake -B build -G "Unix Makefiles" .
+cd build
+cmake --build . --config Release
+.\build\make_sound
+```
+
---
+++ make_sound.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Animal
+{
+public:
+
+    virtual ~Animal()
+    {}
+
+    virtual void MakeSound() const = 0;
+};
+
+class Dog : public Animal
+{
+public:
+    
+    virtual ~Dog()
+    {}
+
+    void MakeSound() const override
+    {
+        std::cout << "Woof" << std::endl;
+    }
+};
+
+class Sheep : public Animal
+{
+public:
+
+    virtual ~Sheep()
+    {}
+
+    void MakeSound() const override
+    {
+        std::cout << "Baaa" << std::endl;
+    }
+};
+
+
+class Lion : public Animal
+{
+public:
+
+    virtual ~Lion()
+    {}
+
+    void MakeSound() const override
+    {
+        std::cout << "Roar" << std::endl;
+    }
+};
+int main(int argc, char* argv[])
+{
+    std::vector<std::unique_ptr<Animal>> animals;
+    animals.emplace_back(std::make_unique<Dog>());
+    animals.emplace_back(std::make_unique<Sheep>());
+    animals.emplace_back(std::make_unique<Lion>());
+
+    for(auto& animal : animals)
+    {
+        animal->MakeSound();
+    }
+}
